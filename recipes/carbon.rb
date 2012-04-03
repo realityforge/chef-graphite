2b9a60a18da58233b6dd87c45f266b978b3c684b
Rob Harrop <rob.harrop@gmail.com>
2012-04-03 15:36:21 +0100

    Tweak the service name

--- recipes/carbon.rb
+++ recipes/carbon.rb
@@ -49,13 +49,13 @@ directory "/opt/graphite/lib/twisted/plugins/" do
   group node['apache']['group']
 end
 
-template "/etc/init.d/carbon" do
+template "/etc/init.d/carbon-cache" do
   owner "root"
   group "root"
   mode "0755"
   source "carbon.init.erb"
 end
 
-service "carbon" do
+service "carbon-cache" do
   action :enable
 end
