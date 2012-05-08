f78a4e3462f8527d06af8f2cd9e6cdad733bc3a4
Peter Donald <peter@realityforge.org>
2012-05-08 15:39:32 +1000

    Inline some variables

--- recipes/whisper.rb
+++ recipes/whisper.rb
@@ -1,21 +1,19 @@
-version = node[:graphite][:version]
-pyver = node[:graphite][:python_version]
-
-remote_file "/usr/src/whisper-#{version}.tar.gz" do
+remote_file "/usr/src/whisper-#{node[:graphite][:version]}.tar.gz" do
   source node[:graphite][:whisper][:uri]
   checksum node[:graphite][:whisper][:checksum]
 end
 
 execute "untar whisper" do
-  command "tar xzf whisper-#{version}.tar.gz"
-  creates "/usr/src/whisper-#{version}"
+  command "tar xzf whisper-#{node[:graphite][:version]}.tar.gz"
+  creates "/usr/src/whisper-#{node[:graphite][:version]}"
   cwd "/usr/src"
 end
 
 execute "install whisper" do
   command "python setup.py install"
-  creates "/usr/local/lib/python#{pyver}/dist-packages/whisper-#{version}.egg-info"
-  cwd "/usr/src/whisper-#{version}"
+  creates "#{node[:python][:pip][:prefix_dir]}/lib/python#{node[:graphite][:python_version]}/dist-packages/whisper-#{node[:graphite][:version]}.egg-info"
+  cwd "/usr/src/whisper-#{node[:graphite][:version]}"
+end
 
 directory "#{node[:graphite][:base_dir]}" do
   owner node['apache']['user']
@@ -31,6 +29,6 @@ directory "#{node[:graphite][:base_dir]}/bin" do
 end
 
 execute 'install whisper utils' do
-  command "cp /usr/src/whisper-#{version}/bin/* #{node[:graphite][:base_dir]}/bin"
+  command "cp /usr/src/whisper-#{node[:graphite][:version]}/bin/* #{node[:graphite][:base_dir]}/bin"
   creates "#{node[:graphite][:base_dir]}/bin/rrd2whisper.py" # and more
 end
