dc6ec264de4b0f6b09714eba2c3e2413f2418f6f
Peter Donald <peter@realityforge.org>
2012-05-08 15:39:18 +1000

    Make sure that the bin directory is created prior to attempting to copy files into it

--- recipes/whisper.rb
+++ recipes/whisper.rb
@@ -16,6 +16,18 @@ execute "install whisper" do
   command "python setup.py install"
   creates "/usr/local/lib/python#{pyver}/dist-packages/whisper-#{version}.egg-info"
   cwd "/usr/src/whisper-#{version}"
+
+directory "#{node[:graphite][:base_dir]}" do
+  owner node['apache']['user']
+  group node['apache']['group']
+  mode "700"
+  recursive true
+end
+
+directory "#{node[:graphite][:base_dir]}/bin" do
+  owner node['apache']['user']
+  group node['apache']['group']
+  mode "700"
 end
 
 execute 'install whisper utils' do
