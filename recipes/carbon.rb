adf2eff7976bf31e0853b4c6d371c2338d1317d5
Peter Donald <peter@realityforge.org>
2012-05-08 15:35:01 +1000

    Inline some variables

--- recipes/carbon.rb
+++ recipes/carbon.rb
@@ -1,24 +1,21 @@
 package "python-twisted"
 package "python-simplejson"
 
-version = node[:graphite][:version]
-pyver = node[:graphite][:python_version]
-
-remote_file "/usr/src/carbon-#{version}.tar.gz" do
+remote_file "/usr/src/carbon-#{node[:graphite][:version]}.tar.gz" do
   source node[:graphite][:carbon][:uri]
   checksum node[:graphite][:carbon][:checksum]
 end
 
 execute "untar carbon" do
-  command "tar xzf carbon-#{version}.tar.gz"
-  creates "/usr/src/carbon-#{version}"
+  command "tar xzf carbon-#{node[:graphite][:version]}.tar.gz"
+  creates "/usr/src/carbon-#{node[:graphite][:version]}"
   cwd "/usr/src"
 end
 
 execute "install carbon" do
   command "python setup.py install"
-  creates "#{node[:graphite][:base_dir]}/lib/carbon-#{version}-py#{pyver}.egg-info"
-  cwd "/usr/src/carbon-#{version}"
+  creates "#{node[:graphite][:base_dir]}/lib/carbon-#{node[:graphite][:version]}-py#{node[:graphite][:python_version]}.egg-info"
+  cwd "/usr/src/carbon-#{node[:graphite][:version]}"
 end
 
 template "#{node[:graphite][:base_dir]}/conf/carbon.conf" do
