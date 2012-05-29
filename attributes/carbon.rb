46ff4f382256fcfa237645e85ccc69b6b1b30abe
Peter Donald <peter@realityforge.org>
2012-05-29 15:02:25 +1000

    Improve the way we register storage schemas so that it is more compatible with chefs deep merge

--- attributes/carbon.rb
+++ attributes/carbon.rb
@@ -8,12 +8,10 @@ default[:graphite][:carbon][:pickle_receiver][:interface] = "127.0.0.1"
 default[:graphite][:carbon][:pickle_receiver][:port] = 2004
 default[:graphite][:carbon][:cache_query][:interface] = "127.0.0.1"
 default[:graphite][:carbon][:cache_query][:port] = 7002
-default[:graphite][:carbon][:storage_schemas] =
-  [
-    {
-      :name => :catchall,
-      :priority => 0,
-      :pattern => '^.*',
-      :retentions => '10s:6h,1m:7d,10m:5y'
-    }
-  ]
+default[:graphite][:carbon][:storage_schemas] = Mash.new
+default[:graphite][:carbon][:storage_schemas][:catchall] =
+  {
+    :priority => 0,
+    :pattern => '^.*',
+    :retentions => '10s:6h,1m:7d,10m:5y'
+  }
