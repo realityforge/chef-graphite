42eb8a7bc6121de93c66e94c8ec5ed10f5918caa
Peter Donald <peter@realityforge.org>
2012-06-01 10:13:06 +1000

    Indicate that I am the maintainer of this variant

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
-maintainer       "Heavy Water Software Inc."
-maintainer_email "ops@hw-ops.com"
+maintainer       "Peter Donald"
+maintainer_email "peter@realityforge.org"
 license          "Apache 2.0"
 description      "Installs/Configures graphite"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
